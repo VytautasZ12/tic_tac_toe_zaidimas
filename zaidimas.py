b6ba2c3952dc958bd10f8dff3e9708a2661477f8
VytautasZ12 <vzvytautasz@gmail.com>
2023-08-18 13:40:29 +0300

    Išskirta lygiosios funkcija, pakeista pagrindinė žaidimo funkcija.

--- zaidimas.py
+++ zaidimas.py
@@ -1,4 +1,3 @@
-# Lentelės vaizdavimas.
 def lentelės_piešimas(vieta):
     print("      {} | {} | {} ".format(vieta[6], vieta[7], vieta[8]))
     print("\t ___|___|___")
@@ -7,7 +6,7 @@ def lentelės_piešimas(vieta):
     print("      {} | {} | {} ".format(vieta[0], vieta[1], vieta[2]))
     print("\t    |   |   ")
 
-# Rezultatų lentelė, kurioje bus rodomi žaidėjų vardai ir taškai po kiekvieno žaidimo.
+
 def rezultatai(taškai):
     print("\t-------------------")
     print("\t Rezultatų lentelė")
@@ -18,7 +17,8 @@ def rezultatai(taškai):
     print("\t ", žaidėjų_sąrašas[0], "\t", taškai[žaidėjų_sąrašas[0]])
     print("\t ", žaidėjų_sąrašas[1], "\t", taškai[žaidėjų_sąrašas[1]])
     print("\t-------------------\n")
-# Gilimos laimėjimo kombinacijos, horizontaliai, vertikaliai, įstrižai.
+
+
 def laimėjimo_patvirtinimas(žaidėjo_vieta, žaidėjo_ėjimas):
     laimėjimo_kombinacijos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     for kombinacijos in laimėjimo_kombinacijos:
@@ -27,29 +27,42 @@ def laimėjimo_patvirtinimas(žaidėjo_vieta, žaidėjo_ėjimas):
     return False
 
 
+# Funkcija kuri bus vygdoma jei nei vienas žaidėjas nelaimėjo.
+def lygiosios(žaidėjo_vieta):
+    if len(žaidėjo_vieta["X"]) + len(žaidėjo_vieta["O"]) == 9:
+        return True
+    return False
 
-# Leis įvesti žaidėjo vardą,
-# priskirs simboli (X arba 0),
-# suks cikla per lentelę tikrinant ėjimus ir laimėjimo galimybes,
-# atspausdins laimėtoja arba lygiąsias.
-def žaidimas():
-    žaidėjas1 = input("Įveskite pirmo žaidėjo vardą: ")
-    žaidėjas2 = input("Įveskite pirmo žaidėjo vardą: ")
-    print(f" {žaidėjas1}'simbolis yra - X")
-    print(f" {žaidėjas2}'simbolis yra - O")
-    lentelės_piešimas(vieta)
-    for index in range(0, 9):
-        if index % 2 == 0:
-            index = ėjimų_įvedimas(žaidėjas1)
-            vieta[index] = "X"
-        else:
-            index = ėjimų_įvedimas(žaidėjas2)
-            vieta[index] = "0"
 
+def žaidimo_ciklas(žaidėjo_ėjimas):
+    vieta = [" " for vieta in range(9)]
+    žaidėjo_vieta = {"X": [], "O": []}
+    while True:
         lentelės_piešimas(vieta)
-        laimėjimo_galimybės(vieta, žaidėjas1, žaidėjas2)
+        try:
+            įvestas_skaičius = int(input(f"{žaidėjo_ėjimas}, Įveskite skaičių nuo 1 iki 9: "))
+        except ValueError:
+            print("Blogas įvedimas, bandykite dar kartą")
+            continue
+        if įvestas_skaičius < 1 or įvestas_skaičius > 9:
+            print("Blogas įvedimas, bandykite dar kartą")
+            continue
+        if vieta[įvestas_skaičius - 1] != " ":
+            print("Pasirinkta vieta užimta, pasirinkite kitą vietą:")
+            continue
+        vieta[įvestas_skaičius - 1] = žaidėjo_ėjimas
+        žaidėjo_vieta[žaidėjo_ėjimas].append(įvestas_skaičius)
+        if laimėjimo_patvirtinimas(žaidėjo_vieta, žaidėjo_ėjimas):
+            lentelės_piešimas(vieta)
+            print("Jūs laimėjote")
+            return žaidėjo_ėjimas
+        if lygiosios(žaidėjo_vieta):
+            lentelės_piešimas(vieta)
+            print("Lygiosios")
+            return "="
 
-    print("Lygiosios, bandykite dar kartą 🙂")
+        if žaidėjo_ėjimas == "X":
+            žaidėjo_ėjimas = "O"
+        else:
+            žaidėjo_ėjimas = "X"
 
-
-žaidimas()
